eef87ede503bde1ada3c2ec1cd298255ecc03829
kubaejj <jakub.jezbera.023@pslib.cz>
2025-06-05 12:28:48 +0200

    velky obri commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,121 @@
-const {src, dest, watch, series} = require('gulp');
-const nunjucksRender = require('gulp-nunjucks-render');
-const data = require('gulp-data');
-const plumber = require('gulp-plumber');
-const fs = require('fs');
-const path = require('path');
-const colors = require('ansi-colors');
-const fancyLog = require('fancy-log');
-const htmlLint = require('gulp-htmllint');
-const prettyError = require('gulp-prettyerror');
+const { src, dest, watch, series } = require("gulp");
+const nunjucksRender = require("gulp-nunjucks-render");
+const data = require("gulp-data");
+const plumber = require("gulp-plumber");
+const fs = require("fs");
+const path = require("path");
+const colors = require("ansi-colors");
+const fancyLog = require("fancy-log");
+const htmlLint = require("gulp-htmllint");
+const prettyError = require("gulp-prettyerror");
 
-// Cesty ke šablonám a datům
+// --- CSS ---
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+
+// --- JS ---
+const terser = require("gulp-terser");
+
+// --- nexg-gen format ---
+///const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+// Cesty
 const paths = {
-  templates: 'src/**/*.html',
-  dataFile: 'src/data.json',
-  dest: './'
+  templates: "src/**/*.html",
+  dataFile: "src/data.json",
+  dest: "./",
+  styles: "src/styles/**/*.scss",
+  stylesDest: "dist/css",
+  scripts: "src/scripts/*.js",
+  scriptsDest: "dist/js",
+  images: 'src/img/*.{jpg,jpeg}',
+  imagesDest: 'dist/images'
 };
 
-// Načte JSON data s proměnnými
-function getData() {
-  return JSON.parse(fs.readFileSync(paths.dataFile));
-}
-
-// Render task
+// --- Šablony (Nunjucks) ---
 function render() {
-  return src(['src/**/*.html',  '!src/layout.html'])
-
+  return src(["src/**/*.html", "!src/layout.html"])
     .pipe(plumber())
-    .pipe(data(getData))
-    .pipe(nunjucksRender({
-      path: ['src/'] // pro include/extend
-    }))
+    .pipe(data(JSON.parse(fs.readFileSync(paths.dataFile))))
+    .pipe(
+      nunjucksRender({
+        path: ["src/"],
+      })
+    )
     .pipe(dest(paths.dest));
 }
 
-// Watcher
+// --- SCSS → CSS + autoprefix + minify ---
+function styles() {
+  return src(paths.styles)
+    .pipe(plumber())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest(paths.stylesDest));
+}
+
+// --- Minifikace JS ---
+function scripts() {
+  return src(paths.scripts).pipe(plumber()).pipe(terser()).pipe(dest(paths.scriptsDest));
+}
+
+// --- Konverze obrázků na WebP ---
+// function imagesToWebp() {
+//   return src(paths.images)
+//     .pipe(plumber())
+//     .pipe(webp())
+//     .pipe(dest(paths.imagesDest));
+// }
+
+// --- Konverze obrázků na AVIF ---
+function imagesToAvif() {
+  return src(paths.images)
+    .pipe(plumber())
+    .pipe(avif({ quality: 50 }))
+    .pipe(dest(paths.imagesDest));
+}
+
+// --- Watcher ---
 function watchFiles() {
   watch(paths.templates, render);
   watch(paths.dataFile, render);
+  watch(paths.styles, styles);
+  watch(paths.scripts, scripts);
+  watch(paths.images, series(/*imagesToWebp,*/ imagesToAvif));
 }
 
+// --- HTML lint ---
 function htmlLintTask() {
-    let errorFree = true;
+  let errorFree = true;
 
-    return src(['src/**/*.html'])
-        .pipe(
-            htmlLint({}, function (filepath, issues) {
-                if (issues.length > 0) {
-                    errorFree = false;
-                    fancyLog(colors.cyan('[gulp-htmllint]') + ' Error in ' + colors.magenta(filepath));
-                }
+  return src(["src/**/*.html"])
+    .pipe(
+      htmlLint({}, function (filepath, issues) {
+        if (issues.length > 0) {
+          errorFree = false;
+          fancyLog(colors.cyan("[gulp-htmllint]") + " Error in " + colors.magenta(filepath));
+        }
 
-                issues.forEach(function (issue) {
-                    process.stdout.write(
-                        colors.white('line ' + issue.line + ', col ' + issue.column) +
-                            '\t\t' +
-                            colors.red(issue.msg) +
-                            ' ' +
-                            colors.white('[' + issue.rule + ':' + issue.code + ']') +
-                            '\n'
-                    );
-                });
-
-                if (issues.length > 0) {
-                    process.exitCode = 1;
-                }
-            })
-        )
-        .on('finish', function (error) {
-            if (errorFree) {
-                fancyLog(colors.green('Task completed successfully!') + ' [_htmlLint]');
-            } else {
-                fancyLog(colors.red('Task failed!'));
-            }
+        issues.forEach(function (issue) {
+          process.stdout.write(colors.white("line " + issue.line + ", col " + issue.column) + "\t\t" + colors.red(issue.msg) + " " + colors.white("[" + issue.rule + ":" + issue.code + "]") + "\n");
         });
+
+        if (issues.length > 0) {
+          process.exitCode = 1;
+        }
+      })
+    )
+    .on("finish", function () {
+      if (errorFree) {
+        fancyLog(colors.green("Task completed successfully!") + " [_htmlLint]");
+      } else {
+        fancyLog(colors.red("Task failed!"));
+      }
+    });
 }
 
-
-exports.default = series(render, htmlLintTask, watchFiles);
+// --- Default ---
+exports.default = series(render, styles, scripts, htmlLintTask, watchFiles);
